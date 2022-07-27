7c40b032c6df24a3212bf7ecb828568e72457a08
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2022-07-27 16:10:21 +0200

    R Package Epigraf

--- epigraf/R/convertTest.R
+++ epigraf/R/convertTest.R
@@ -1,25 +1,25 @@
-#
-# Converter functions to upload social media data to Epigraf ----
-#
 
-library(tidyverse)
-
-#
-# Convert text to epigraf article
-#
+#' Convert text to epigraf article
 #' @param text Dataframe with the columns id, project, caption and content
 #' @return Dataframe with article, section and item
+#' @export
 
-textToArticle <- function(text) {
 
+textToArticle <- function(text)
+  { if (!requireNamespace("tidyverse", quietly = TRUE)) {
+    stop(
+      "Package \"tidyverse\" must be installed to use this function.",
+      call. = FALSE
+    )
+    }
   projects <- tibble(
     table="projects",
     type="default",
     id= paste0("projects-int",unique(text$project)),
     name=unique(text$project)
   )
-  
-  
+
+
   articles <- tibble(
     table="articles",
     type="default",
@@ -27,7 +27,7 @@ textToArticle <- function(text) {
     projects_id=paste0("projects-int",text$project),
     name=text$caption,
   )
-  
+
   sections <- tibble(
     table="sections",
     type="default",
@@ -35,7 +35,7 @@ textToArticle <- function(text) {
     articles_id= paste0("articles-int",text$id),
     name="Text"
   )
-  
+
   items <- tibble(
     table="items",
     type="default",
@@ -44,14 +44,14 @@ textToArticle <- function(text) {
     articles_id= paste0("articles-int",text$id),
     content=text$content
   )
-  
+
   bind_rows(
     projects,
     articles,
     sections,
     items
   )
-  
+
 }
 
 # Example
@@ -63,3 +63,4 @@ textToArticle(
     content="Worte sind Buchstaben mit Kleber"
   )
 )
+
