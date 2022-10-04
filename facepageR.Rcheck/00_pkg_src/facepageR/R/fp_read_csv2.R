ed4a6bd5d51a49d921f018cb8155344b143732f9
strohne <jakob.juenger@uni-muenster.de>
2022-10-04 12:58:01 +0200

    Clean up

--- facepageR.Rcheck/00_pkg_src/facepageR/R/fp_read_csv2.R
+++
@@ -1,9 +0,0 @@
-# Function to read a CSV file
-#' @import tidyverse
-#' @param
-#' @keywords
-#' @examples
-#' @export
-fp_read_csv2 <- function(filename) {
-  read_csv2(filename,na="None")
-}
--- facepageR.Rcheck/00_pkg_src/facepageR/R/fp_status.R
+++
@@ -1,9 +0,0 @@
-#' Function to get number of levels, querytypes, querystatus and objecttypes
-#' @param
-#' @keywords
-#' @examples
-#' @export
-fp_status <- function(data) {
-  data %>%
-    count(level,querytype,querystatus,objecttype)
-}
