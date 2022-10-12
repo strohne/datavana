1286dd192b2d2d17d000ded4d89fddbbec19c66b
isklee <isabel.kleefeld@stud.uni-greifswald.de>
2022-10-12 14:31:21 +0200

    documentation

--- facepageR/R/facepageR.R
+++ facepageR/R/facepageR.R
@@ -1,8 +1,11 @@
 #' @description
 #' To learn more about the facepageR package, start with the vignettes:
-#' `browseVignettes(package = "facepageR")`
-#' They are divided into load and prepare data, basics and utilities.
+#' `browseVignettes(package = "facepageR")`.
+#' They are divided into:
+#' load and prepare data,
+#' understand the data structure and
+#' get the details of your query.
 #' They may help you to understand the package better and show you how to use it best.
 #' @author Jakob Jünger
 #' @seealso Useful links: tbc
-"_PACKAGE"
+
