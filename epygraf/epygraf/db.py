2b6ec4d59bedb0929756189d5cd38efbea929037
strohne <jakob.juenger@uni-muenster.de>
2023-08-01 20:41:15 +0200

    Add db setup

--- epygraf/epygraf/db.py
+++ epygraf/epygraf/db.py
@@ -1,23 +1,36 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-def connect(db="epi_all"):
+settings = {
+    'host': 'localhost',
+    'port': 3306,
+    'username': 'root',
+    'password': 'root',
+    'database': "epi_all"
+}
+
+def setup(host="localhost", port=3306, username="root", password="root", database=""):
+    """
+    Prepare the database connection parameters
+    """
+    settings['host'] = host
+    settings['port'] = port
+    settings['username'] = root
+    settings['password'] = password
+    settings['database'] = database
+
+def connect(db = "epi_all"):
     """
     Create an engine used to connect to the database
     
     Parameters:
     - db The database name
     """
-    user = 'root'
-    pw = 'root'
-    host = 'localhost'
-    port = 3306
-
     db_str = 'mysql+pymysql://' + \
-             user + ':' + \
-             pw + '@' + \
-             host + ':' + \
-             str(port) + '/' + \
+             settings['username'] + ':' + \
+             settings['password'] + '@' + \
+             settings['host'] + ':' + \
+             str(settings['port']) + '/' + \
              db
 
     db_enginge = create_engine(db_str)
