6e99c0cd59e09cb9c490367076b6760a4f2d2a2e
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2023-08-17 12:17:51 +0200

    add api funtions to epygraf package

--- epygraf/build/lib/epygraf/db.py
+++ epygraf/build/lib/epygraf/db.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 from sqlalchemy import create_engine, text
+from urllib.parse import urlparse, urlunparse
 
 settings = {
     'host': 'localhost',
@@ -15,7 +17,7 @@ def setup(host="localhost", port=3306, username="root", password="root", databas
     """
     settings['host'] = host
     settings['port'] = port
-    settings['username'] = root
+    settings['username'] = username
     settings['password'] = password
     settings['database'] = database
 
@@ -64,3 +66,59 @@ def table(table="", db="epi_all"):
         db_rows = pd.read_sql(text(query), con=con)
 
     return(db_rows)
+
+
+  
+def api_setup(apiserver, apitoken, verbose=False): 
+    
+    """ 
+    Save API connection settings to environment variables
+    """
+    
+    settings = dict(locals())
+    settings = {f"epi_{key}": str(value) for key, value in settings.items()}
+    os.environ.update(settings)
+
+
+def api_buildurl(endpoint, query=None, database=None, extension="json"):
+    
+    """
+    Build base URL
+    """
+
+    # Get server and token from the global settings
+    server = os.getenv("epi_apiserver")
+    token = os.getenv("epi_apitoken")
+    verbose = os.getenv("epi_verbose") == "TRUE"
+
+    parsed_url = urlparse(server)
+    parsed_query = dict(parsed_url.query)
+
+    parsed_query["token"] = token
+    
+    # Add query parameters
+    if query is not None:
+        parsed_query.update(query)
+
+    if not endpoint.startswith("/"):
+        endpoint = "/" + endpoint
+
+    if extension is None:
+        extension = ""
+    elif extension and not extension.startswith("."):
+        extension = "." + extension
+
+    if database is not None:
+        path = f"epi/{database}{endpoint}{extension}"
+    else:
+        path = f"{endpoint}{extension}"
+
+    parsed_url = parsed_url._replace(path=path, query="")
+
+    url = urlunparse(parsed_url)
+
+    if verbose:
+        print(url)
+
+    return url
+
