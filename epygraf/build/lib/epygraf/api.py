eefe10763904f1bac06ef3ff22a71ca080f8ba00
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2023-08-21 12:26:42 +0200

    Add Api function

--- epygraf/build/lib/epygraf/api.py
+++ epygraf/build/lib/epygraf/api.py
@@ -1,6 +1,4 @@
-import pandas as pd
 import os
-from sqlalchemy import create_engine, text
 from urllib.parse import urlparse, urlunparse
 
 
--- epygraf/build/lib/epygraf/db.py
+++ epygraf/build/lib/epygraf/db.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import os
 from sqlalchemy import create_engine, text
-from urllib.parse import urlparse, urlunparse
 
 settings = {
     'host': 'localhost',
