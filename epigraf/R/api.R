e6d0caf06446d581e9e08d3c1b56c712e425d5a0
strohne <krimskram@freenet.de>
2024-02-11 11:09:07 +0100

    Add transfer function to epigraf package

--- epigraf/R/api.R
+++ epigraf/R/api.R
@@ -303,7 +303,17 @@ api_job_execute <- function(job_id) {
   return (invisible((polling == F) & (error == F)))
 }
 
-
+#' Transfer datasets between different databases
+#'
+#' @param table Table name, e.g. "types"
+#' @param db_source Source database name
+#' @param db_target Target database name
+#' @param db_params A parameter list for selecting the appropriate rows.
+#'                  For example: params <- list("scopes" = "properties")
+api_transfer <- function(table, db_source, db_target, db_params = list()) {
+  db_params["source"] = db_source
+  api_job_create(paste0(table, "/transfer"), db_params, db_target)
+}
 
 #' Patch data
 #'
